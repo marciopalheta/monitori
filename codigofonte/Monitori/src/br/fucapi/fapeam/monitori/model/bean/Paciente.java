518b513a4842eaabe176eb62b8820bac58b59152
marciopalheta <marcio.palheta@gmail.com>
2014-01-22 16:09:04 -0400

    Inclusao da classe paciente

---
+++ codigofonte/Monitori/src/br/fucapi/fapeam/monitori/model/bean/Paciente.java
@@ -0,0 +1,33 @@
+package br.fucapi.fapeam.monitori.model.bean;
+
+@SuppressWarnings("serial")
+public class Paciente extends Usuario {
+	private boolean hipertenso;
+	private boolean diabetico1;
+	private boolean diabetico2;
+
+	public boolean isHipertenso() {
+		return hipertenso;
+	}
+
+	public void setHipertenso(boolean hipertenso) {
+		this.hipertenso = hipertenso;
+	}
+
+	public boolean isDiabetico1() {
+		return diabetico1;
+	}
+
+	public void setDiabetico1(boolean diabetico1) {
+		this.diabetico1 = diabetico1;
+	}
+
+	public boolean isDiabetico2() {
+		return diabetico2;
+	}
+
+	public void setDiabetico2(boolean diabetico2) {
+		this.diabetico2 = diabetico2;
+	}
+
+}

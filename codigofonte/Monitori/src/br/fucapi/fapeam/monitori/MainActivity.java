4c98737098e9a8fc81d080f74363506bb098e816
marciopalheta <marcio.palheta@gmail.com>
2014-01-22 14:35:17 -0400

    Carga inicial de arquivos do projeto

---
+++ codigofonte/Monitori/src/br/fucapi/fapeam/monitori/MainActivity.java
@@ -0,0 +1,22 @@
+package br.fucapi.fapeam.monitori;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainlayout);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}

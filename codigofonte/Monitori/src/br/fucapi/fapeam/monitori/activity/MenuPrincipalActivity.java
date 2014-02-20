5c008ef8cc3d420d19196a234ce02dc3771e7002
Luene <Luene@PC-Luene>
2014-02-20 18:27:28 -0300

    atualizando menu

--- codigofonte/Monitori/src/br/fucapi/fapeam/monitori/activity/MenuPrincipalActivity.java
+++ codigofonte/Monitori/src/br/fucapi/fapeam/monitori/activity/MenuPrincipalActivity.java
@@ -9,29 +9,29 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MenuPrincipalActivity extends Activity implements OnClickListener {
-	private Button btEsportivos;
+	private Button btmedico;
 	private Button btAgente;
-	private Button btLuxo;
-	private Button btSobre;
+	private Button btpaciente;
+	private Button btcoletadados;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Layout do Dashboard
 		setContentView(R.layout.menuprincipal);
-		btEsportivos = (Button) findViewById(R.id.btEsportivos);
-		btEsportivos.setOnClickListener(this);
+		btmedico = (Button) findViewById(R.id.btmedico);
+		btmedico.setOnClickListener(this);
 		btAgente = (Button) findViewById(R.id.btAgente);
 		btAgente.setOnClickListener(this);
-		btLuxo = (Button) findViewById(R.id.btLuxo);
-		btLuxo.setOnClickListener(this);
-		btSobre = (Button) findViewById(R.id.btSobre);
-		btSobre.setOnClickListener(this);
+		btpaciente = (Button) findViewById(R.id.btpaciente);
+		btpaciente.setOnClickListener(this);
+		btcoletadados = (Button) findViewById(R.id.btcoletadados);
+		btcoletadados.setOnClickListener(this);
 	}
 	@Override
 	public void onClick(View v) {
 		
       
-        	Intent intent = new Intent(this, PacienteDadosActivity.class);
+        	Intent intent = new Intent(this, PacienteActivity.class);
     		if(v == btAgente) {
     			startActivity(intent);
     		}
